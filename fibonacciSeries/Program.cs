dd4ec29f9deb06e68c9e9909f6e9731db89e656f
Gaurav Gautam <gauravgautam@i-techsoftware.com>
2021-02-23 11:57:49 -0500

    Implemented the app

---
+++ fibonacciSeries/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace fibbonicSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter number of fibbonic terms");
+                int n = int.Parse(Console.ReadLine());
+
+
+                int i = 0, j = 1;
+                if (n == 1)
+                {
+                    Console.Write("\nfibonacci series is : " + i);
+                }
+                else
+                {
+                    Console.Write("\nfibonacci series is : " + i + " " + j + " ");
+                    for (int k = 2; k < n; k++)
+                    {
+                        int x = i + j;
+                        i = j;
+                        j = x;
+                        Console.Write(x);
+                        Console.Write("  ");
+                    }
+                }
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
